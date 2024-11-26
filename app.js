a1ec74337f3264f3822790906984e362b2302f3f
Pourya Zardosht <pooriyazardosht.812@gmail.com>
2024-11-26 20:08:45 +0330

    feat: exception handler added

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const SwaggerConfig = require("./src/config/swagger.config");
 const mainRouter = require("./src/app.routes");
+const NotFoundHandler = require("./src/common/exception/notfound-handler");
+const AllExceptionHandler = require("./src/common/exception/all-exception.handler");
 
 dotenv.config();
 
@@ -11,8 +13,10 @@ async function main() {
   require("./src/config/mongoose.config");
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  SwaggerConfig(app);
   app.use(mainRouter);
+  SwaggerConfig(app);
+  NotFoundHandler(app);
+  AllExceptionHandler(app);
   app.listen(port, () => {
     console.log(`server: http://localhost:${port}`);
   });
