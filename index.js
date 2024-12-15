a634a9e70f05388dd10ed592790d49750d3a1e9b
Pourya Zardosht <pooriyazardosht.812@gmail.com>
2024-12-15 11:29:22 +0330

    feat: cors add

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 const baseURL = process.env.BASE_URL;
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  })
+    cors({
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    })
 );
 require("./src/config/mongoose.config");
 
 app.use(mainRouter);
 app.listen(port, () => {
-  console.log(`server: ${baseURL}:${port}`);
+    console.log(`server: ${baseURL}:${port}`);
 });
